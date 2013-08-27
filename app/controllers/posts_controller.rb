069a0f653200ea4b65ed9bf4cfb2c2b59c030c78
PaulaEs <paulaproxy@gmail.com>
2013-08-27 18:50:50 +0200

    Update and edit added.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,31 @@ class PostsController < ApplicationController
 		@post = Post.new(post_params)
 		
 		if @post.save
-			redirect_to @post
+			redirect_to @post, notice: 'Post was successfully created.'
 		else
-			render 'new'
+			render action: 'new'
 		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post, notice: 'Post was successfully updated.'
+		else
+			render action: 'edit'
+		end
+	end
+
+
 
 	private
 		def post_params
