4791035aaceb3ddec1dc41f9bcd24c2ad4f8a5fd
PaulaEs <paulaproxy@gmail.com>
2013-08-28 01:26:57 +0200

    Basic authentication added.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	http_basic_authenticate_with name: 'me', password: 'onrails', only: :destroy
 
 	def create
 		@post = Post.find(params[:post_id])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	http_basic_authenticate_with name: 'me', password: 'onrails', except: [:index, :show] 
 
 	def index
 		@posts = Post.all
