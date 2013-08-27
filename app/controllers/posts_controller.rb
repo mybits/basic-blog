de7ddd0b22eb5b46fdc3c861cc00372b94be142c
PaulaEs <paulaproxy@gmail.com>
2013-08-27 02:13:50 +0200

    Validations and error messages added.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,19 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
-	
+
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
 		
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
